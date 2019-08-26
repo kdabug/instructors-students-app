bf0ebd8acd8d26c8f3d9ca4634e6567826c41477
Mikayda Mills <mills.ma12@gmail.com>
2019-08-26 15:19:57 -0400

    client add app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import "./App.css";
+
+class App extends Component {
+  state = {
+    instructors: [],
+    selectedInstructor: null
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:3000/instructors").then(response => {
+      this.setState({
+        instructors: response.data.instructors
+      });
+    });
+  }
+
+  selectInstructor = instructor => {
+    this.setState({
+      selectedInstructor: instructor
+    });
+  };
+
+  instructors = () => {
+    return this.state.instructors.map(instructor => (
+      <div key={instructor.id}>
+        <Link
+          to={`/instructors/${instructor.id}`}
+          onClick={() => this.selectInstructor(instructor)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {instructor.name}
+        </Link>
+        <hr />
+      </div>
+    ));
+  };
+
+  instructor = () => {
+    const instructor = this.state.selectedInstructor;
+    return (
+      <div>
+        <img alt={instructor.name} src={instructor.photo} width="20%" />
+        <h1>{instructor.name}</h1>
+        <p>{instructor.description}</p>
+        <a href={instructor.link}>Connect</a>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Link exact="true" to="/">
+            Course App
+          </Link>
+          <Switch>
+            <Route exact path="/" render={this.instructors} />
+            <Route exact path="/instructors/:id" render={this.instructor} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
